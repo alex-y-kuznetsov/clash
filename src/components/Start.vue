4dc1edcf6670278b58b5e6a02052b821aa77eea8
Alex <alex.y.kuznetsov@gmail.com>
2020-12-24 19:17:52 +0500

    first commit

---
+++ src/components/Start.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hello">
+        {{ text }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'Hello World'
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.hello {
+    color: tomato;
+}
+</style>
