e940a605d62b40602cd94f6448528e4981524572
Alex <alex.y.kuznetsov@gmail.com>
2021-01-09 19:27:27 +0500

    typo fixes

--- src/data/rules.js
+++ src/data/rules.js
@@ -4,6 +4,6 @@ export default [
   'Press "Next" to proceed to encounters',
   '"Venture" to face your encounter',
   'You can "Fight" your encounter as long as you have stamina',
-  'You can retreat at any moent to regenerate stamina and health',
+  'You can retreat at any moment to regenerate stamina and health or switch characters',
   'When your health reaches zero your character is defeated'
 ]
